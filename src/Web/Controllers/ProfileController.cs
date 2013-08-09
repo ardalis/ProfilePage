fa7c4fa9c88b9387e37f63e97d3cb5d9e2f5c793
Steve Smith <steve@kentsmiths.com>
2013-08-09 15:36:21 -0400

    working on getting profile page to look right

--- src/Web/Controllers/ProfileController.cs
+++ src/Web/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -12,7 +13,18 @@ namespace Web.Controllers
             {
                 return View("Leaderboard");
             }
-            return View();
+            var model = new ProfileViewModel()
+            {
+                Name = "Ardalis",
+                RealName = "Steve Smith",
+                ShortBio = "EVP, Services, Telerik",
+                BlogName = "Ardalis.com",
+                BlogUrl = "http://ardalis.com",
+                TwitterAlias="ardalis",
+                FacebookUrl="http://facebook.com/StevenAndrewSmith",
+                LinkedInUrl = "http://www.linkedin.com/in/stevenandrewsmith/"
+            };
+            return View(model);
         }
 
     }
---
+++ src/Web/Models/ProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ProfileViewModel
+    {
+        public string Name { get; set; }
+        public string RealName { get; set; }
+        public string ShortBio { get; set; }
+        public string BlogName { get; set; }
+        public string BlogUrl { get; set; }
+        public string TwitterAlias { get; set; }
+        public string FacebookUrl { get; set; }
+        public string LinkedInUrl { get; set; }
+    }
+}
