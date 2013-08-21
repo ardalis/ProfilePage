ae19a75fc8422390b2e61301ad2f3aaba3a39763
Steve Smith <steve@kentsmiths.com>
2013-08-20 22:11:43 -0400

    working on having acts trigger stuff
    Logging acts should trigger points on profiles, which in turn should
    fire off NewLevelAchievedEvents...

--- src/Core/Model/Activity.cs
+++ src/Core/Model/Activity.cs
@@ -11,5 +11,6 @@ namespace Core.Model
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int PointValue { get; set; }
     }
 }
---
+++ src/Core/Services/ProfileLevelMonitorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Core.Services
+{
+    public class ProfileLevelMonitorService
+    {
+        private readonly ActivityService _activityService;
+
+        public ProfileLevelMonitorService(ActivityService activityService)
+        {
+            this._activityService = activityService;
+            this._activityService.Logged += _activityService_Logged;
+        }
+
+        void _activityService_Logged(object sender, ActLoggedEventArgs e)
+        {
+            var act = e.Act;
+
+            // update profile from act
+
+            // check for level increase and fire event if level increased
+        }
+
+
+    }
+}
