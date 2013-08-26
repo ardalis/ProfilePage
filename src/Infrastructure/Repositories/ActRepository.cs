80c4ab816d422045c55d9a9177b8660cbb307f99
Steve Smith <steve@kentsmiths.com>
2013-08-26 15:48:04 -0400

    Adding infrastructure and initial repositories

---
+++ src/Infrastructure/Repositories/ActRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Interfaces;
+using Core.Model;
+
+namespace Infrastructure.Repositories
+{
+    public class ActRepository : IActRepository
+    {
+        private static Dictionary<Guid, Act> _acts = new Dictionary<Guid, Act>();
+        public Act Get(Guid id)
+        {
+            return _acts[id];
+        }
+
+        public void Save(Act act)
+        {
+            if (_acts.ContainsKey(act.Id))
+            {
+                _acts[act.Id] = act;
+            }
+            else
+            {
+                _acts.Add(act.Id, act);
+            }
+        }
+    }
+}
