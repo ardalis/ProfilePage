5254976bd05a9621a8ea982be8e21570a7e8601c
Steve Smith <steve@kentsmiths.com>
2013-08-28 16:25:33 -0400

    adding main processor service

---
+++ src/ProfileEngine/Core/Services/ActivityProcessorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileEngine.Core.Services
+{
+    /// <summary>
+    /// This service monitors a queue of activities and applies them to the appropriate profile.
+    /// </summary>
+    public class ActivityProcessorService
+    {
+        public ActivityProcessorService()
+        {
+
+        }
+        /// <summary>
+        /// Retrieves the next event from the activity queue and applies it to the appropriate profile
+        /// </summary>
+        public void ProcessActivityQueueItem()
+        {
+        }
+    }
+}
